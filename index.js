91cdd1fffe7f8a1d07c2c5d45e07071f1273102f
jiangleo <jhwleo@gmail.com>
2022-04-11 18:35:49 +0800

    feat: iOS 新架构支持 M1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/2_Component/App.js';
+import App from './src/02_Component/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
